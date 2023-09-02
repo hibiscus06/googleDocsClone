7300c58ee34e554abecebb69c87ec6ccd1bac186
Rudrika Singh <72783505+hibiscus06@users.noreply.github.com>
2023-09-03 00:31:52 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,12 @@
+const mongoose = require("mongoose")
+const Document = require("./Document")
 
+mongoose.connect("mongodb://localhost/googledocsclone", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useFindAndModify: false,
+//   useCreateIndex: true,
+})
 
 const io = require('socket.io')(3001, {
     cors:{
@@ -7,14 +15,39 @@ const io = require('socket.io')(3001, {
     }
 })
 
-io.on("connection", socket => {
+const defaultValue = ""
 
-    socket.on('send-changes', delta => {
-        console.log(delta) 
+io.on("connection", socket => {
+    socket.on('get-document',async documentId => {    //loads up the data 
+     const document = await findOrCreateDocument(documentId)
+     socket.join(documentId)
+     socket.emit("load-document",document.data)
+     
+     socket.on('send-changes', delta => {
+     socket.broadcast.to(documentId).emit("receive-changes",delta) // here we are saying on our current socket we want to broadcast the changes to everyone except us and the changes are passed as delta 
+    })  
     
-    socket.broadcast.emit("receive-changes",delta) // here we are saying on our current socket we want to broadcast the changes to everyone except us and the changes are passed as delta 
+    socket.on("save-document",async data => {
+        await Document.findByIdAndUpdate( documentId ,{data})
     })
+  })
 })
+
+async function findOrCreateDocument(id)
+{
+    if(id == null)
+    return 
+
+    const document = await Document.findById(id)
+    if (document) return document
+
+    return await Document.create({_id: id , data: defaultValue})
+}
+
+
+
+
+
 
 
 
